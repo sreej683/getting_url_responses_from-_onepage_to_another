5ab494f80254a374cc6586a33491528e4a409ea5
B.Jayasree <j7226642@gmail.com>
2024-06-30 22:45:12 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def lasya(request):
+    return render(request,'lasya.html')
+
+
+def Akarsh(request):
+    return render(request,'Akarsh.html')
